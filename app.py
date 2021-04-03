2186fbede83a19b3e122734353c638c6bc16b1a3
Aravinda <68860525+Aravinda214@users.noreply.github.com>
2021-04-04 01:53:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    #int_features = [float(x) for x in request.form.values()]
-    int_features= [0,2,1,2,3,2,3,2,3,2]
+    int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     output = prediction[0]
@@ -31,29 +30,3 @@ def predict_api():
 
     output = prediction[0]
     return jsonify(output)"""
-"""import numpy as np
-from flask import Flask, request, render_template
-import pickle
-app = Flask(__name__)
-model=pickle.load(open('model.pkl','rb'))
-@app.route('/')
-def home():
-    return render_template('index.html')
-#@app.route('/predict',methods=['POST'])
-@app.route('/predict',methods=['POST','GET'])
-def predict():
-    int_features=[x for x in request.form.get('table', 0)]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
-
-    if output>str(0.5):
-        return render_template('index.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
-    else:
-        return render_template('index.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
- """
-   
-#int_features = [2,3,2,3,3,2,3,3,3,1]
-    #int_features= [0,0,0,0,0,0,0,0,0,0]
