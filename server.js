0b7b943380f01972c96127c32b463d8e5829ebc2
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2023-10-06 01:50:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,57 +41,7 @@ app.get('/Images/*', function(req, res) {
   res.sendFile(path.join(__dirname, req.path))
 })
 
-// Function and GET for sentiment analysis
-document.addEventListener('DOMContentLoaded', speechToEmotion, false)
-
-function speechToEmotion() {
-  const recognition = new webkitSpeechRecognition()
-  recognition.lang = 'en-US'
-  recognition.continuous = true
-
-  recognition.onresult = function(event) {
-    const results = event.results
-    const transcript = results[results.length-1][0].transcript
-
-    setEmoji('searching')
-
-    fetch(`/emotion?text=${transcript}`)
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.score > 0) {
-          setEmoji('positive')
-        } else if (result.score < 0) {
-          setEmoji('negative')
-        } else {
-          setEmoji('neutral')
-        }
-      })
-      .catch((e) => {
-        console.error('Request error -> ', e)
-        recognition.abort()
-      })
-  }
-
-  recognition.onerror = function(event) {
-    console.error('Recognition error -> ', event.error)
-    setEmoji('error')
-  }
-
-  recognition.onaudiostart = function() {
-    setEmoji('listening')
-  }
-
-  recognition.onend = function() {
-    setEmoji('idle')
-  }
-
-  recognition.start();
-
-  function setEmoji(type) {
-    const emojiElem = document.querySelector('.emoji img')
-    emojiElem.classList = type
-  }
-}
+// GET for sentiment analysis
 
 
 app.get('/emotion', function(req, res) {
