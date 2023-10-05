ffd483ff4e85b7d9f33e01ca5239b421697c6caf
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2023-10-05 23:57:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
+app.get('/Suicide%20Analysis.html', function(req, res) {
+  res.sendFile(path.join(__dirname, 'Suicide Analysis.html'))
+})
+
+app.get('/About.html', function(req, res) {
+  res.sendFile(path.join(__dirname, 'About.html'))
+})
+
+app.get('/speech%20to%20text.html', function(req, res) {
+  res.sendFile(path.join(__dirname, 'speech%20to%20text.html'))
+})
+
+app.get('/depression_test.html.html', function(req, res) {
+  res.sendFile(path.join(__dirname, 'depression_test.html'))
+})
+
+app.get('/Helpline.html', function(req, res) {
+  res.sendFile(path.join(__dirname, 'Helpline.html'))
+})
+
 app.get('/emotion', function(req, res) {
   const sentiment = new Sentiment()
   const text = req.query.text
